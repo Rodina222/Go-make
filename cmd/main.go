19a33aecf563003d54624678045474e8c2e6e2a4
Rodina Mohamed <75133923+Rodina222@users.noreply.github.com>
2023-07-19 16:11:24 +0300

    formatting

--- cmd/main.go
+++ cmd/main.go
@@ -11,26 +11,16 @@ const CommandFormat = "./gomake -f Makefile -t target"
 
 func main() {
 
-	graph := internal.NewGraph()
-
 	target, filePath, err := internal.ParseCommandLine()
-
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to parse command line: %v\n", err)
 		fmt.Println(CommandFormat)
 		os.Exit(1)
 	}
 
-	// parse the Makefile (create the graph)
-	err = graph.ParseMakeFile(filePath)
+	graph := internal.NewGraph()
 
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	err = graph.Execute(target)
-
+	err = graph.Execute(filePath, target)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
