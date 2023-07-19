a595d33793334bac11f3796fee07fb63cce5b17e
Rodina Mohamed <75133923+Rodina222@users.noreply.github.com>
2023-07-19 17:33:53 +0300

    gofmt formatting

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,7 @@ const CommandFormat = "gomake -f Makefile -t target"
 
 func main() {
 
+	// parse command line
 	target, filePath, err := internal.ParseCommandLine()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to parse command line: %v\n", fmt.Errorf("%w", err))
@@ -18,6 +19,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	// create new graph
 	graph := internal.NewGraph()
 
 	// call execute method of the graph
